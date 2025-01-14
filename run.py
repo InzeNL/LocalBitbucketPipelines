ecc69eb89bdea7f9da3a84bf4e9e6af38d0a4200
Inze Bielderman <Bielderman@InzeNL.com>
2025-01-14 21:24:58 +0100

    Add validation for required property 'pipelines'

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 import yaml
 
 document = yaml.load(open("bitbucket-pipelines.yml").read(), Loader=yaml.CLoader)
+
+if document["pipelines"] is None:
+    print("Missing required property 'pipelines'")
+
+    exit(1)
