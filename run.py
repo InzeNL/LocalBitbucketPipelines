d927a3cdb1b5f4fa3f48d7c7f83840541e7badd0
Inze Bielderman <Bielderman@InzeNL.com>
2025-01-14 21:25:23 +0100

    Add validation for required properties of 'pipelines' property

--- run.py
+++ run.py
@@ -6,3 +6,13 @@ if document["pipelines"] is None:
     print("Missing required property 'pipelines'")
 
     exit(1)
+    
+pipelines = document["pipelines"]
+
+if "default" not in pipelines \
+    and "branches" not in pipelines \
+    and "tags" not in pipelines \
+    and "bookmarks" not in pipelines \
+    and "custom" not in pipelines \
+    and "pull-requests" not in pipelines:
+        print("'pipelines' requires at least a default, branches, tags, bookmarks or custom section")
