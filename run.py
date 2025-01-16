06bbfbbb7c5e799b70dee66e6d4a513cd07151ea
Inze Bielderman <Bielderman@InzeNL.com>
2025-01-16 18:27:20 +0100

    Add schema validator

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import yaml
 import argparse
+import jsonschema
+import json
 
 parser = argparse.ArgumentParser(
     prog='Bitbucket Pipelines Runner',
@@ -8,21 +10,6 @@ parser = argparse.ArgumentParser(
 parser.parse_args()
 
 document = yaml.load(open("bitbucket-pipelines.yml").read(), Loader=yaml.CLoader)
+schema = json.loads(open("schema.json").read())
 
-if document["pipelines"] is None:
-    print("Missing required property 'pipelines'")
-
-    exit(1)
-    
-pipelines = document["pipelines"]
-
-if "default" not in pipelines \
-    and "branches" not in pipelines \
-    and "tags" not in pipelines \
-    and "bookmarks" not in pipelines \
-    and "custom" not in pipelines \
-    and "pull-requests" not in pipelines:
-        print("'pipelines' requires at least a default, branches, tags, bookmarks or custom section")
-
-        exit(1)
-
+jsonschema.validate(document, schema)
