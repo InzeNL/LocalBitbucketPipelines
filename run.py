064a414f4d070025a933a6851671b2ff6fa977b7
Inze Bielderman <Bielderman@InzeNL.com>
2025-01-16 19:58:15 +0100

    Add function to start, execute, and kill step

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import argparse
 import jsonschema
 import json
 import shutil
+import subprocess
 
 #region Arguments
 parser = argparse.ArgumentParser(
@@ -37,6 +38,27 @@ def get_steps(pipelines):
     return steps
 #endregion
 
+#region Docker
+def docker_start_step(image):
+    step_result = subprocess.run(["docker", "run", "-di", image], stdout=subprocess.PIPE, text=True)
+
+    return step_result.stdout.splitlines()[0]
+
+def docker_execute_step(container_id, step):
+    if isinstance(step, list):
+        for sub_step in step:
+            docker_execute_step(container_id, sub_step)
+    elif "script" in step:
+        script = step["script"]
+        for command in script:
+            executable_command = "docker exec -i " + container_id + " " + command
+            subprocess.run(executable_command)
+
+def docker_kill_step(container_id):
+    subprocess.run(["docker", "kill", container_id], stdout=subprocess.DEVNULL)
+    subprocess.run(["docker", "rm", container_id], stdout=subprocess.DEVNULL)
+#endregion
+
 if shutil.which("docker") is None:
     print("Docker needs to be installed for this program to work")
 
@@ -62,5 +84,11 @@ if arguments.default:
         exit(1)
     
     default = pipelines["default"]
-    
-    print("Running default pipeline")
+
+    steps = get_steps(default)
+
+    for step in steps:
+        container_id = docker_start_step(image)
+        docker_execute_step(container_id, step)
+
+        docker_kill_step(container_id)
