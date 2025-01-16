2642de24a39b314574416c3492767dcfc89f9778
Inze Bielderman <Bielderman@InzeNL.com>
2025-01-16 18:34:42 +0100

    Add argument for default pipeline

--- run.py
+++ run.py
@@ -3,13 +3,24 @@ import argparse
 import jsonschema
 import json
 
+#region Arguments
 parser = argparse.ArgumentParser(
     prog='Bitbucket Pipelines Runner',
     description='Allows users to run Bitbucket Pipelines locally')
 
-parser.parse_args()
+parser.add_argument(
+    "-d", "--default",
+    action="store_true",
+    help="Run the default pipeline"
+)
+
+arguments = parser.parse_args()
+#endregion
 
 document = yaml.load(open("bitbucket-pipelines.yml").read(), Loader=yaml.CLoader)
 schema = json.loads(open("schema.json").read())
 
 jsonschema.validate(document, schema)
+
+if arguments.default:
+    print("Running default pipeline")
